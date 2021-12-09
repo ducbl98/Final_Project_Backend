76fc9a3bebd611755a1d14c12dd59b8bcf23a578
ducbl98 <ductit1998@gmail.com>
2021-12-09 08:19:27 +0700

    Day 1

---
+++ database/migrations/2021_12_08_155824_create_seekers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSeekersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('seekers', function (Blueprint $table) {
+            $table->id();
+            $table->string('gender');
+            $table->string('birthday');
+            $table->string('phone_number');
+            $table->string('address')->nullable();
+            $table->string('education')->nullable();
+            $table->string('experience')->nullable();
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('seekers');
+    }
+}
